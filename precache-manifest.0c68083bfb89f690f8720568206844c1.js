8c6c810c49891c408fbd8b3f1c9a053788dd9332
Akshat Kumar Agarwal <akshat.16@outlook.com>
2020-05-31 22:20:35 +0530

    Updates

---
+++ precache-manifest.0c68083bfb89f690f8720568206844c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7594dccca6edd63d37b13e06cbb26fe1",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "910ef85efba6dde7d890",
+    "url": "/chat-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ecb735992b507d5c0db2",
+    "url": "/chat-app/static/js/2.00c3760c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat-app/static/js/2.00c3760c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910ef85efba6dde7d890",
+    "url": "/chat-app/static/js/main.4a4a524b.chunk.js"
+  },
+  {
+    "revision": "80a61362ae1886d1f8d1",
+    "url": "/chat-app/static/js/runtime-main.cd3cebf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
